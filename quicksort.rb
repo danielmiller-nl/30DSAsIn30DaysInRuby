08bf6e0969b65d2866b6a8da3835c7a9b632e853
Daniel Miller <danielmiller.nl@gmail.com>
2015-04-30 14:55:41 -0230

    quickSort almost fixed

--- quicksort.rb
+++ quicksort.rb
@@ -1,26 +1,23 @@
 def quickSort array,lo=0,hi=array.length
-  
+  return if lo+1 >= hi
   def swap array,a,b 
     temp = array[a]
     array[a] = array[b]
     array[b] = temp
   end
 
-  return if lo == hi
-
   left = lo+1
   right = hi-1
 
-  puts "A #{array} #{left} #{right} #{lo} #{hi}"
   until left>=right
-
-    left+=1 if array[left]<array[0]
-    right-=1 if array[right]>array[0]
-    swap array,left,right if array[left]>=array[0]&&array[right]<=array[0]
+    left+=1 until array[left]>=array[lo]
+    right-=1 until array[right]<=array[lo]
+    swap array,left,right unless left>right
+    #left+=1 if array[left]<array[lo]
+    #right-=1 if array[right]>array[lo]
   end
   
-  swap array,lo,right if array[lo]>array[right]
-  puts "B #{array} #{left} #{right} #{lo} #{hi}"
+  swap array,lo,right
   quickSort array,lo,right
   quickSort array,right+1,hi
 
